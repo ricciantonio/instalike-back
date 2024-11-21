72daef1ba66f3ec3f2b543ae83f382f671965afe
ricciantonio <ricciiantonioo.gmail.com>
2024-11-21 15:24:32 -0300

    mongodbconnection

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from "express"; 
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.listen(3000, () =>{
-    console.log("Servidor escutando...");
-});
 
-app.get("/api", (req, res)=>{
-    res.status(200).send("Boas vindas à imersão!");
+routes(app);
+
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
 });
