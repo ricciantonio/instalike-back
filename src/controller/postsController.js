4e962191abbc1831ccedc9ab11d7964224df17cd
ricciantonio <ricciiantonioo.gmail.com>
2024-11-25 16:25:01 -0300

    adicionar api para auterar os posts put

--- src/controller/postsController.js
+++
@@ -1,7 +0,0 @@
-import { getTodosPosts } from "../models/postsModels.js";
-
-export async function listarPosts (req, res)
-{
-    const posts =  await getTodosPosts();
-    res.status(200).json(posts);
-}
